d7762efaa36676b76c21852bc2e4c368a022dc81
Vince <vince321c@gmail.com>
2025-02-28 02:23:04 -0500

    Removed retry logic

--- clicker.py
+++ clicker.py
@@ -8,15 +8,13 @@ def click(image):
   gui.moveTo(left, top)
   gui.click()
 
-retries = 0
-while retries < 5:
+while True:
   try:
     download_button = gui.locateOnScreen("assets/download-button.png", confidence=0.5)
     endorsement_reminder = gui.locateOnScreen("assets/endorsement-reminder.png", confidence=0.5)
 
     click(download_button)
     click(endorsement_reminder)
-    retries = 0 # Reset retries after success
   except:
     # To simplify things, we don't care about not being able
     # to find images. We'll just retry after a second
